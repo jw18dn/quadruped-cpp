1b3bcd71a0dfaa5d742e85d9b18f9f5f34fbf453
jw18dn <jw18dn@fsu.edu>
2023-11-24 14:51:53 -0500

    Removing bazel, adding cmake

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jason/Repos/quadruped_make/main.cpp" "/home/jason/Repos/quadruped_make/build/CMakeFiles/main.dir/main.cpp.o"
+  "/home/jason/Repos/quadruped_make/src/components.cpp" "/home/jason/Repos/quadruped_make/build/CMakeFiles/main.dir/src/components.cpp.o"
+  "/home/jason/Repos/quadruped_make/src/gait.cpp" "/home/jason/Repos/quadruped_make/build/CMakeFiles/main.dir/src/gait.cpp.o"
+  "/home/jason/Repos/quadruped_make/src/optimization.cpp" "/home/jason/Repos/quadruped_make/build/CMakeFiles/main.dir/src/optimization.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/src/components.cpp.o"
+  "CMakeFiles/main.dir/src/gait.cpp.o"
+  "CMakeFiles/main.dir/src/optimization.cpp.o"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
