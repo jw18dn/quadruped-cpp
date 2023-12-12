769ff083ed8e5c09d162258df3080857fb7a6253
jw18dn <jw18dn@fsu.edu>
2023-12-12 10:43:38 -0500

    edits to cmake

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 Mujoco humanoid example using docker
 
+For new build folders-----
+1. mkdir build, cd in to folder
+2. cmake ..
+
 Steps----------------
 1. ./run_dev.sh to build the container
 2. press F1, and attach to enter the container
 3. In new window, change directory to build
 4. run make
-5. run ./Excectuable Name
+5. run ./Excectuable_Name
 6. "docker ps" to show process' running or "exit" to kill
 
 
--- build/CMakeFiles/main.dir/DependInfo.cmake
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -1,17 +1,14 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
   )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/jason/Repos/quadruped_cpp/main.cpp" "/home/jason/Repos/quadruped_cpp/build/CMakeFiles/main.dir/main.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "../include"
-  "/usr/include/eigen3"
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/app/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.o" "gcc" "CMakeFiles/main.dir/src/main.cpp.o.d"
   )
 
 # Targets to which this target links.
